93c837b2d51f2fbacfae4c9650a209948218ba72
Ankit <aidev.ankit@gmail.com>
2023-07-06 18:06:26 +0530

    Wappler (2)

---
+++ Wappler/Wappler Guides/public/dmxAppConnect/config.js
@@ -0,0 +1,20 @@
+dmx.config({
+  "index": {
+    "query": [
+      {
+        "type": "text",
+        "name": "myparam"
+      },
+      {
+        "type": "text",
+        "name": "hello"
+      }
+    ],
+    "global": [
+      {
+        "type": "text",
+        "name": "hello"
+      }
+    ]
+  }
+});
