2fba567192c31f665a5af9cd5b21243350a914d1
Ankit <aidev.ankit@gmail.com>
2023-07-11 18:11:45 +0530

    Wappler Frot end

--- Wappler/Wappler Guides/public/dmxAppConnect/config.js
+++ Wappler/Wappler Guides/public/dmxAppConnect/config.js
@@ -16,5 +16,83 @@ dmx.config({
         "name": "hello"
       }
     ]
+  },
+  "cookies": {
+    "cookies": [
+      {
+        "type": "text",
+        "name": "Notice"
+      },
+      {
+        "type": "text",
+        "name": "octa"
+      }
+    ]
+  },
+  "passingvalues": {
+    "sessionStorage": [
+      {
+        "type": "array",
+        "name": "data"
+      },
+      {
+        "type": "object",
+        "name": "headers"
+      },
+      {
+        "type": "text",
+        "name": "aaa"
+      },
+      {
+        "type": "text",
+        "name": "jai"
+      },
+      {
+        "type": "text",
+        "name": "second"
+      },
+      {
+        "type": "array",
+        "name": "cars"
+      },
+      {
+        "type": "text",
+        "name": "first"
+      }
+    ]
+  },
+  "page21": {
+    "sessionStorage": [
+      {
+        "type": "array",
+        "name": "data"
+      },
+      {
+        "type": "object",
+        "name": "headers"
+      },
+      {
+        "type": "text",
+        "name": "aaa"
+      },
+      {
+        "type": "text",
+        "name": "jai"
+      },
+      {
+        "type": "text",
+        "name": "second"
+      },
+      {
+        "type": "text",
+        "name": "first"
+      }
+    ]
+  },
+  "arrays": {
+    "repeat1": {
+      "meta": null,
+      "outputType": "array"
+    }
   }
 });
